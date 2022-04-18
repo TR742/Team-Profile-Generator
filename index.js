279e6f9123f68052cb3bb34ac3c8b3e2f89db0bf
TR742 <Tyrich1442@gmail.com>
2022-04-17 22:11:14 -0700

    Inquirer Prompt functional for manager

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter the full name of the Team manager',
+        name: 'manager-name'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the Team manager employee ID',
+        name: 'manager-id'
+    },
+    {
+        type: 'input',
+        message: 'Pleae enter the Team manager email address',
+        name: 'manager-email',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the Team manager office number',
+        name: 'manager-number',
+    }
+]
+
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput);
+    })
+}
+
+init();
